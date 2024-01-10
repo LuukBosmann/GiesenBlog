af33495fc5b9384611a8feda48413da774b8e99b
YannickWhorst <abounce21@gmail.com>
2024-01-10 13:07:54 +0100

    Een andere template gebruikt

---
+++ laravel/resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<script>
+import Layout  from "../Layout/Layout.vue";
+
+export default {
+    components: { Layout }
+}
+</script>
+
+<template>
+    <Layout>
+        <h1>Test</h1>
+    </Layout>
+</template>
+
+<style scoped>
+
+</style>
---
+++ laravel/routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+
+Route::get('/', function () {
+    return Inertia::render('Home');
+});
+
+Route::get('/users', function () {
+    return Inertia::render('Users');
+});
+
+Route::get('/settings', function () {
+    return Inertia::render('Settings');
+});

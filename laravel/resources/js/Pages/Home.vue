ef8ecd58910c44f538d06ed6bf8c2b78f4597bc0
LuukBosmann <Luuk.Bosmann@student.graafschapcollege.nl>
2024-01-17 16:50:45 +0100

    homepage changes

--- laravel/resources/js/Pages/Home.vue
+++ laravel/resources/js/Pages/Home.vue
@@ -1,11 +1,21 @@
 <script setup>
 import Layout from "../Layout/Layout.vue";
+import { Link } from '@inertiajs/inertia-vue3';
 
 </script>
 
 <template>
     <Layout>
+      <div class="container mx-auto mt-16 text-center">
+        <h1 class="text-4xl font-bold mb-8 text-giesenBlue">Welcome to Our Blog Platform</h1>
+        <p class="text-lg text-gray-700 mb-8">Discover and share amazing stories from our community of bloggers.</p>
+        <Link href="/blogs">
+          <button class="bg-giesenBlue text-giesenWhite px-6 py-3 rounded-full text-xl hover:bg-giesenDarkBlue">
+            Explore Blogs
+          </button>
+        </Link>
+      </div>
     </Layout>
-</template>
+  </template>
 
 <style scoped></style>
